bd1844ab0cda5ea09682eacec0e36b030d33f9d5
Sumin Byeon <suminb@gmail.com>
2016-11-13 17:45:35 +0900

    Comply with Flake8

--- textrank.py
+++ textrank.py
@@ -1,5 +1,6 @@
 """
-From this paper: https://web.eecs.umich.edu/~mihalcea/papers/mihalcea.emnlp04.pdf
+From this paper:
+    https://web.eecs.umich.edu/~mihalcea/papers/mihalcea.emnlp04.pdf
 
 External dependencies: nltk, numpy, networkx
 
@@ -12,12 +13,15 @@ import itertools
 import networkx as nx
 import os
 
-#apply syntactic filters based on POS tags
+
+# apply syntactic filters based on POS tags
 def filter_for_tags(tagged, tags=['NN', 'JJ', 'NNP']):
     return [item for item in tagged if item[1] in tags]
 
+
 def normalize(tagged):
     return [(item[0].replace('.', ''), item[1]) for item in tagged]
+
 
 def unique_everseen(iterable, key=None):
     "List unique elements, preserving order. Remember all elements ever seen."
@@ -36,8 +40,11 @@ def unique_everseen(iterable, key=None):
                 seen_add(k)
                 yield element
 
+
 def lDistance(firstString, secondString):
-    "Function to find the Levenshtein distance between two words/sentences - gotten from http://rosettacode.org/wiki/Levenshtein_distance#Python"
+    """Function to find the Levenshtein distance between two words/sentences -
+    gotten from http://rosettacode.org/wiki/Levenshtein_distance#Python
+    """
     if len(firstString) > len(secondString):
         firstString, secondString = secondString, firstString
     distances = range(len(firstString) + 1)
@@ -47,17 +54,20 @@ def lDistance(firstString, secondString):
             if char1 == char2:
                 newDistances.append(distances[index1])
             else:
-                newDistances.append(1 + min((distances[index1], distances[index1+1], newDistances[-1])))
+                newDistances.append(1 + min((distances[index1],
+                                             distances[index1 + 1],
+                                             newDistances[-1])))
         distances = newDistances
     return distances[-1]
 
+
 def buildGraph(nodes):
-    "nodes - list of hashables that represents the nodes of the graph"
-    gr = nx.Graph() #initialize an undirected graph
+    """nodes - list of hashables that represents the nodes of the graph"""
+    gr = nx.Graph()  # initialize an undirected graph
     gr.add_nodes_from(nodes)
     nodePairs = list(itertools.combinations(nodes, 2))
 
-    #add edges to the graph (weighted by Levenshtein distance)
+    # add edges to the graph (weighted by Levenshtein distance)
     for pair in nodePairs:
         firstString = pair[0]
         secondString = pair[1]
@@ -66,11 +76,12 @@ def buildGraph(nodes):
 
     return gr
 
+
 def extractKeyphrases(text):
-    #tokenize the text using nltk
+    # tokenize the text using nltk
     wordTokens = nltk.word_tokenize(text)
 
-    #assign POS tags to the words in the text
+    # assign POS tags to the words in the text
     tagged = nltk.pos_tag(wordTokens)
     textlist = [x[0] for x in tagged]
 
@@ -80,24 +91,30 @@ def extractKeyphrases(text):
     unique_word_set = unique_everseen([x[0] for x in tagged])
     word_set_list = list(unique_word_set)
 
-   #this will be used to determine adjacent words in order to construct keyphrases with two words
+    # this will be used to determine adjacent words in order to construct
+    # keyphrases with two words
 
     graph = buildGraph(word_set_list)
 
-    #pageRank - initial value of 1.0, error tolerance of 0,0001, 
+    # pageRank - initial value of 1.0, error tolerance of 0,0001,
     calculated_page_rank = nx.pagerank(graph, weight='weight')
 
-    #most important words in ascending order of importance
-    keyphrases = sorted(calculated_page_rank, key=calculated_page_rank.get, reverse=True)
+    # most important words in ascending order of importance
+    keyphrases = sorted(calculated_page_rank, key=calculated_page_rank.get,
+                        reverse=True)
 
-    #the number of keyphrases returned will be relative to the size of the text (a third of the number of vertices)
+    # the number of keyphrases returned will be relative to the size of the
+    # text (a third of the number of vertices)
     aThird = len(word_set_list) // 3
-    keyphrases = keyphrases[0:aThird+1]
+    keyphrases = keyphrases[0:aThird + 1]
 
-    #take keyphrases with multiple words into consideration as done in the paper - if two words are adjacent in the text and are selected as keywords, join them
-    #together
+    # take keyphrases with multiple words into consideration as done in the
+    # paper - if two words are adjacent in the text and are selected as
+    # keywords, join them together
     modifiedKeyphrases = set([])
-    dealtWith = set([]) #keeps track of individual keywords that have been joined to form a keyphrase
+    # keeps track of individual keywords that have been joined to form a
+    # keyphrase
+    dealtWith = set([])
     i = 0
     j = 1
     while j < len(textlist):
@@ -109,18 +126,20 @@ def extractKeyphrases(text):
             dealtWith.add(firstWord)
             dealtWith.add(secondWord)
         else:
-            if firstWord in keyphrases and firstWord not in dealtWith: 
+            if firstWord in keyphrases and firstWord not in dealtWith:
                 modifiedKeyphrases.add(firstWord)
 
-            #if this is the last word in the text, and it is a keyword,
-            #it definitely has no chance of being a keyphrase at this point    
-            if j == len(textlist)-1 and secondWord in keyphrases and secondWord not in dealtWith:
+            # if this is the last word in the text, and it is a keyword, it
+            # definitely has no chance of being a keyphrase at this point
+            if j == len(textlist) - 1 and secondWord in keyphrases and \
+                    secondWord not in dealtWith:
                 modifiedKeyphrases.add(secondWord)
 
         i = i + 1
         j = j + 1
 
     return modifiedKeyphrases
+
 
 def extractSentences(text):
     sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
@@ -129,16 +148,18 @@ def extractSentences(text):
 
     calculated_page_rank = nx.pagerank(graph, weight='weight')
 
-    #most important sentences in ascending order of importance
-    sentences = sorted(calculated_page_rank, key=calculated_page_rank.get, reverse=True)
+    # most important sentences in ascending order of importance
+    sentences = sorted(calculated_page_rank, key=calculated_page_rank.get,
+                       reverse=True)
 
-    #return a 100 word summary
+    # return a 100 word summary
     summary = ' '.join(sentences)
     summaryWords = summary.split()
     summaryWords = summaryWords[0:101]
     summary = ' '.join(summaryWords)
 
     return summary
+
 
 def writeFiles(summary, keyphrases, fileName):
     "outputs the keyphrases and summaries to appropriate files"
@@ -156,7 +177,7 @@ def writeFiles(summary, keyphrases, fileName):
     print("-")
 
 
-#retrieve each of the articles
+# retrieve each of the articles
 articles = os.listdir("articles")
 for article in articles:
     print('Reading articles/' + article)
