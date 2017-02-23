7d8c6420292dadab9443d0105c3771f0f1560062
Colton Allen <cmanallen90@gmail.com>
2017-02-23 16:18:36 -0500

    Using find_packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 setup(
@@ -10,6 +10,7 @@ setup(
     author_email='',
     url='http://github.com/suminb/textrank',
     install_requires=['networkx>=1.11.0', 'nltk>=3.2.1', 'numpy>=1.11.2', 'click>=6.6'],
+    packages=find_packages(),
     entry_points='''
         [console_scripts]
         textrank=main:cli
