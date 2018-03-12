96835b6378bc7e36519e12023fa0ce4cea3f6dd7
Jesse O'Connor <jessexoc@gmail.com>
2018-03-13 10:26:42 +1100

    Add editdistance requirement to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Unknown',
     author_email='',
     url='http://github.com/suminb/textrank',
-    install_requires=['networkx>=1.11.0', 'nltk>=3.2.1', 'numpy>=1.11.2', 'click>=6.6'],
+    install_requires=['networkx>=1.11.0', 'nltk>=3.2.1', 'numpy>=1.11.2', 'click>=6.6', 'editdistance==0.4'],
     packages=find_packages(),
     entry_points='''
         [console_scripts]
